74783b0b7fb597b53c4dd336e9bb1ef3d6ea66ef
sanarahtos <4sarahsantos@gmail.com>
2023-06-02 16:23:46 -0700

    edit main menu with pet name

--- MainMenu.java
+++ MainMenu.java
@@ -1,11 +1,14 @@
 
 public class MainMenu
 {
+    public static String petName;
     public static void main (String[] args){
         Pet p = new Pet();
         int choice;
 
-        System.out.println("Congratulations on your new puppy!");
+        System.out.println("Congratulations on your new pet! \nWhat would you like to name them?");
+        petName = CheckInput.getString();
+
         while ((choice = menuOpt()) != 3){
             if(choice == 1){
                 System.out.println(p.giveFood());
@@ -21,7 +24,7 @@ public class MainMenu
      * @return menu choice from user
      */
     public static int menuOpt(){
-        System.out.println("What would you like to do?" +
+        System.out.println("What would you like to do with " + petName + "?" +
                 "\n1. Feed \n2. Play \n3. Quit");
         return CheckInput.getIntRange(1, 3);
     }
